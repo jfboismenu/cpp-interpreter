1e22c5dfe9eaf7ceebd4b55521b523a3f04a75d7
Jean-Francois Boismenu <jf.boismenu@epicgames.com>
2023-04-15 23:38:17 -0400

    finding lines

--- luno.cpp
+++ luno.cpp
@@ -1,6 +1,10 @@
 // Copyright (c) 2023 Jean-François Boismenu
 
+#include <fstream>
+#include <iostream>
 #include <memory>
+#include <string>
+#include <vector>
 
 namespace luno
 {
@@ -10,17 +14,21 @@ namespace line_parser
 class File
 {
   public:
-    const char *const filename;
-    const char *const content;
+    const std::string filename;
+    std::string content;
 };
 
 class Line
 {
   public:
     const File *const file;
-    const char *const start;
+    char *start;
     const int length;
-    const int number;
+    const int line_number;
+    std::string content() const
+    {
+        return std::string(start, length);
+    }
 };
 
 } // namespace line_parser
@@ -29,10 +37,70 @@ class Line
 
 using namespace luno::line_parser;
 
-int main(int, char **)
+File read_file(const char *filename)
 {
-    File file{"filename.txt", "content.txt"};
-    Line line{&file, "filename.txt", 10, 10};
-    (void)line;
+    std::ifstream file(filename);
+    assert(file.is_open());
+    const int length(file.seekg(0, std::ios_base::end).tellg());
+    file.seekg(0, std::ios_base::beg);
+    std::string content(length, '\0');
+    file.read(content.data(), length);
+    return File{std::string(filename), content};
+}
+
+class TranslationUnit
+{
+  public:
+    std::vector<Line> lines;
+};
+
+// class Token {
+//     const char *const start;
+//     const char *const end;
+// };
+
+enum class PreprocessorState
+{
+    start_of_line,
+    identifier,
+    decimal,
+    octal,
+    binary,
+    floating,
+    hexadecimal,
+    directive,
+};
+
+void populate_translation_unit(File &file, TranslationUnit &unit)
+{
+    char *start = file.content.data();
+    int line_no = 0;
+    for (size_t i = 0; i < file.content.size(); ++i)
+    {
+        if (file.content[i] == '\n')
+        {
+            unit.lines.emplace_back(Line{&file, start, int(&file.content[i] - start), line_no});
+            ++line_no;
+            start = &file.content[i] + 1;
+        }
+    }
+    if (&file.content.back() != start)
+    {
+        unit.lines.emplace_back(Line{&file, start, int(&file.content.back() - start) + 1, line_no});
+    }
+}
+
+int main(int, char **argv)
+{
+    std::cout << argv[1] << std::endl;
+    File file(read_file(argv[1]));
+    std::cout << file.content << std::endl;
+    TranslationUnit tu;
+    populate_translation_unit(file, tu);
+    assert(tu.lines.size() == 5);
+    assert(tu.lines[0].content() == "int i = 0;");
+    assert(tu.lines[1].content() == "const char* j = \"this is a \\n \\\" string\";");
+    assert(tu.lines[2].content() == "int k = 0x1234;");
+    assert(tu.lines[3].content() == "char l = 'c';");
     return 0;
 }
---
+++ test.cpp
@@ -0,0 +1,7 @@
+// Copyright (c) 2023 Jean-François Boismenu
+
+int i = 0;
+const char *j = "this is a \n \" string";
+int k = 0x1234;
+char l = 'c';
+bool m = true;
